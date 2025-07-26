e802f1faa482f416ad9c67c0f431dbec639c8362
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-07-26 11:53:27 -0400

    Aprediendo C#

---
+++ Primeros Pasos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeros_Pasos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo"); /* Nos permite mostrar informacion por consola */
+            Console.ReadKey(); /* Esto es para que la consola no se cierre hasta darle a una tecla */
+        }
+    }
+}
