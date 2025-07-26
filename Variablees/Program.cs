7da2fabab99ec8a68b5a6d9eaee5af98923a1df9
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-07-26 14:38:15 -0400

    Teoria Variables

---
+++ Variablees/Program.cs
@@ -0,0 +1,57 @@
+﻿using System; // namespace definido por el sistema
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablees // namespace propio
+{
+    /*  Las variables son espacios que se guardan en la memoria del pc.
+     *  y su tamaño dependera de lo que queremos guardar.
+     *  
+     *  Para indicarle al pc que deseamos usar una variable, debemos declararla
+     *  
+     *  Para declarar una variables podemos usar:
+     *  
+     *  Letras mayusculas, letras minusculas, numeros y guiones.
+     *  
+     *  Nunca podemos usar numeros antes de letras, signos a excepcion de 
+     *  guiones.
+     *  
+     *  
+     *  Tipos de Variables:
+     *  
+     *  String: Cadena de caracteres, letras, numeros, etc.
+     *  
+     *  int: Cuando queremos guardar valores numericos enteros con signo, en el 
+     *  rango de -2,147,483,648 hasta 2,147,483,647
+     *  
+     *  Long: Guando queremos guardar valores enteros numéricos grandes
+     *  con un rango desde 9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807
+     *  
+     *  Short: Guardar valores numéricos enteros, pero su rango es menor
+     *  y varia desde -32,768 hasta 32,767.
+     *  
+     *  Byte: Puede guardar un byte de información, Esto equivale a un valor entero
+     *  positivo entre 0 y 255.
+     *  
+     *  Decimal: Este tipo puede guardar un valor numerico con decimales. Su rango
+     *  es de ±1.0 hasta ±7.9
+     *  
+     *  Float: Otro tipo muy utilizado para guardar valores numericos decimales. Para
+     *  este tipo el rango es de ±1.5 hasta ±3.4
+     *  
+     *  Bool: Es una variable booleana, es decir que solamente puede guardar 
+     *  los valores verdadero o falso (true o false)
+     *  
+     */
+
+
+    internal class Program // Clase 
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
