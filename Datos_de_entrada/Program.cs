c0ffcb0fccab5dc437603d0210f6d17eb071f69d
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-07-27 12:30:42 -0400

    Datos de entrada 1

---
+++ Datos_de_entrada/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos_de_entrada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir datos al usuario
+            try
+            {
+                string nombre;
+                byte edad;
+                float altura;
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine(); // Console.ReadLine() Permite que el usuario ingrese datos al sistema
+                Console.WriteLine("Ingresa tu edad");
+                edad = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Ingresa tu altura");
+                altura = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("El nombre ingresado es {0} y su edad es {1} y mi altura es {2}", nombre, edad, altura);
+               
+            }catch (FormatException)
+            {
+                Console.WriteLine("El tipo de dato ingresado no es correcto");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("El valor ingresado es menor o mayor al limite permitido");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error {0}", ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Datos_de_entrada/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Datos_de_entrada")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Datos_de_entrada")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("9687b208-be89-4ff9-a783-8ee3dc6d6340")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
