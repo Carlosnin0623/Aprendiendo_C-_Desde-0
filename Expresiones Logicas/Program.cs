b23fe82a8e46d509452a1f3edf6105c6f565eb02
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-07-28 17:30:54 -0400

    Expresiones Logicas 1

---
+++ Expresiones Logicas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones_Logicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tanque = 40;
+            int recorrido = 250;
+
+            if(tanque < 50 && recorrido > 200)
+            {
+                Console.WriteLine("Las expresiones son verdaderas, tanquear el vehiculo");
+            }
+            else
+            {
+                Console.WriteLine("Las expresiones no se cumplen, false");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
