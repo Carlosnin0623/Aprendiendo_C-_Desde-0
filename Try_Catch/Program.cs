c9fd098cd47cc3f8214533867ca1160e76607499
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-07-31 17:02:05 -0400

    Try Catch

--- Try_Catch/Program.cs
+++ Try_Catch/Program.cs
@@ -10,6 +10,43 @@ namespace Try_Catch
     {
         static void Main(string[] args)
         {
+            /*   Try Catch
+             *   
+             *   Cuando se presenta un error al momento de ingresar datos, la aplicación
+             *   se va a detener. Esta estructura nos va a permitir mostrar el error avisandole
+             *   al usuario que ingreso un formato no valido.
+             *   
+             *   try{
+             *      // Codigo
+             *   
+             *    }catch(Exeption "Tipos de variables de Exepciones) {
+             *
+             *    } 
+             *
+             *
+             */
+
+            int n1, n2, resultado;
+
+            try
+            {
+                Console.WriteLine("Ingresa el primer numero");
+                n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el segundo numero");
+                n2 = Convert.ToInt32(Console.ReadLine());
+
+                resultado = n1 * n2;
+
+                Console.WriteLine("El resultado es {0}", resultado);
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error {0}", ex.Message);
+            }
+
+         
+
+            Console.ReadKey();
         }
     }
 }
