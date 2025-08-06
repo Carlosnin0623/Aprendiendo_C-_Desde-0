0a4a9f3d09bd57c8dad7ee8afdfed72ac0345b2a
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-08-06 14:10:28 -0400

    ForEach

---
+++ 15-ForEach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Academia Ciencia";
+
+            foreach (char letra in nombre)
+            {
+                Console.WriteLine("{0}", letra);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
