005c33109f76120ed26d67b2335ce9152c60e810
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-08-10 19:01:19 -0400

    Bloque de ejercicio 3

---
+++ 20-Bloque_de_ejericios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _20_Bloque_de_ejericios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Escribe un programa que tome un número entero y sume todos sus dígitos.
+             */
+
+            int numero;
+            string resultado;
+            int resultado1 = 0;
+            string texto;
+
+            Console.WriteLine("Ingrese el número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            texto = Convert.ToString(numero);
+
+            foreach(char letra in texto)
+            {
+               resultado = Convert.ToString(letra);
+               resultado1 += Convert.ToInt32(resultado);
+            }
+
+            Console.WriteLine("El resultado es {0}", resultado1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
