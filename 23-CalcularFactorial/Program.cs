90dbfb7f5e249a91d3b1a775024378d2052ce99c
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-08-21 17:02:48 -0400

    Calcular Factorial

---
+++ 23-CalcularFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_CalcularFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicio 2: Calcular factorial
+             
+              Crea un programa que calcule el factorial de un número ingresado por el usuario utilizando un método.
+              El programa debe solicitar un número entero no negativo al usuario y mostrar el factorial correspondiente.
+              Reglas:
+
+              El método secundario debe ser de tipo long
+
+              El método secundario debe declarar una variable de tipo int en los atributos
+
+              Recuerda:  Factorial es multiplicar todos los números enteros positivos que hay entre el número propuesto
+              (en este caso el ingresado por el usuario) y el 1 
+
+             */
+        }
+    }
+}
