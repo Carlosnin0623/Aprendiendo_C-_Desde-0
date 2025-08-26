49b5e4943a136315726ce318da7ae27e4e989d2e
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-08-26 16:05:06 -0400

    27 Ejercicio Array Bidimencional

---
+++ 27-Ejercicio-Array-Bidimencional/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Ejercicio_Array_Bidimencional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion Array Bidimencional ejercicio - Matriz
+            int[,] matriz = {
+                            {1,2,3},
+                            {4,5,6},
+                            {7,8,9}
+                            };
+
+            int suma = 0;
+
+            for(int i = 0; i < matriz.GetLength(0); i++) // matriz.GetLength(0): Obtiene el número de columnas
+            {
+                for(int j = 0; j < matriz.GetLength(1); j++) // // matriz.GetLength(1): Obtiene el número de filas
+                {
+                    suma += matriz[i,j];
+                }
+            }
+
+            Console.WriteLine("La suma de los valores de la matriz es: {0}", suma);
+
+            Console.ReadKey();
+        }
+    }
+}
