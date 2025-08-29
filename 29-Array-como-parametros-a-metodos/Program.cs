1cc7a027287eb6a5df32300463fa20916fad69d0
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-08-29 16:56:44 -0400

    29 Arreglos como metodos

---
+++ 29-Array-como-parametros-a-metodos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Array_como_parametros_a_metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Arreglos como parámetros de métodos
+             * 
+             * Cuando enviamos un arreglo como parámetro a un método:
+             * 
+             *  El arreglo enviado y el arreglo del parámetro deben ser el mismo tipo de dato.
+             *  
+             *  Deben tener las mismas dimensiones, es decir ambos son como lista o como matriz.
+             *  
+             *  Ambos deben ser el mismo estilo: normal o jagged.
+             *  
+             *  Definir un método con arreglo:
+             *  
+             *  static void Imprime(int[] arreglo)
+             *  {
+             *  
+             *     Código
+             *     
+             *  }
+             *  
+             *  Invocación:
+             *  
+             *  int[] numeros = new int[5];
+             *  imprime(numeros);
+             *  
+             *  
+             *  
+             *  
+             *  
+             */
+
+
+        }
+    }
+}
