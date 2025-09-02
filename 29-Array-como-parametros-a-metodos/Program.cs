e2f18ed4e826afdd748751a62b3974f912eea6f0
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-09-02 13:38:22 -0400

    Arreglos como parametros parte 2

--- 29-Array-como-parametros-a-metodos/Program.cs
+++ 29-Array-como-parametros-a-metodos/Program.cs
@@ -41,7 +41,40 @@ namespace _29_Array_como_parametros_a_metodos
              *  
              */
 
+            int numero;
 
+            Console.WriteLine("ingrese la cantidad de números que va a ingresar");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[numero];
+
+            int n = 0;
+
+            Console.WriteLine("Este programa te solicitará {0} números", numeros.Length);
+
+
+            for(n = 0; n < numeros.Length; n++)
+            {
+                Console.WriteLine("Ingresa un número:");
+                numeros[n] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Imprimir(numeros);
+
+
+            Console.ReadKey();
+        }
+
+
+        static void Imprimir(int[] numeros)
+        {
+            Console.WriteLine("El listado de números contenidos en el array son:");
+
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("{0}", numeros[i]);
+            }
         }
     }
 }
